d0feddc658727bcdddfeb109617cfc556a639f00
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-11 20:22:57 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+  def artist_field(song)
+    if song.artist_id.nil? 
+      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+    else
+      hidden_field_tag :artist_id
+    end 
+  end 
 end
