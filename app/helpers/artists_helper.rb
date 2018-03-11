1ee8be80ef020c1ecee3213281b47f7fb5b73952
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-11 20:59:54 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
 
-  def artist_field(song, artist)
+  def artist_select(song, artist)
     if artist
       artist.name
     else
