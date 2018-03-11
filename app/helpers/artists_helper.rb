9502188bf8e91b74f68ddbcdc602000a687137dd
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-11 20:58:01 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -5,7 +5,7 @@ module ArtistsHelper
 
   def artist_field(song, artist)
     if artist
-      hidden_field_tag "song[artist_id]", song.artist_id
+      artist.name
     else
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     end
